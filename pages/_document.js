901e99a0de1bc126524ad06b83b5bc9a4093c444
Puskar Ghimire <puskar93@gmail.com>
2022-05-16 12:43:51 +0545

    project added

---
+++ pages/_document.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Html, Head, Main, NextScript} from 'next/document'
+
+const Document = () => {
+  return (
+    <Html>
+        <Head/>
+        <body className=''>
+          
+            <Main/>
+            <NextScript/>
+        </body>
+    </Html>
+  )
+}
+
+export default Document
